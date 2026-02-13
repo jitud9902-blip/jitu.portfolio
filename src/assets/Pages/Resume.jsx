e8684be1e101c57da724dbab67e8943a02c1fbc8
Jitendra Das <jitud9902@gmail.com>
2026-02-13 17:08:43 +0530

    all file change structure change 10

--- src/assets/Pages/Resume.jsx
+++ src/assets/Pages/Resume.jsx
@@ -6,11 +6,11 @@ function Resume() {
 
     <div style={{width:"500px",height:"800px",marginLeft:"480px",marginTop:"50px"}}  className='resume'>
 
-        <img src='./resume.png'></img> 
+        <img src='/resume.png'></img> 
         
         
         
-        <a href='./resume.png'download>  <button style={{marginLeft:"180px",marginTop:"20px"}} className='btn btn-primary'> DownLoad </button> </a>
+        <a href='/resume.png'download>  <button style={{marginLeft:"180px",marginTop:"20px"}} className='btn btn-primary'> DownLoad </button> </a>
          </div>
 
 
